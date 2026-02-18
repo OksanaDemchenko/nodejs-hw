34264b3b5772555e60c1fd5c05a0b6fcdf1a8621
Demchenko Oksana <sulichka33@gmail.com>
2026-02-18 21:07:48 +0200

    fix: change put to patch for update route

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -12,7 +12,7 @@ const router = Router();
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
-router.put('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', updateNote);
 router.delete('/notes/:noteId', deleteNote);
 
 export default router;
