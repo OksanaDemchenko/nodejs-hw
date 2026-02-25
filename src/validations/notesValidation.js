fc79b82c7f947eda2b535ef20554a1fd272b144c
Demchenko Oksana <sulichka33@gmail.com>
2026-02-25 20:46:47 +0200

    add pagination, filtering, request validation for notes API

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,54 @@
+import { Joi, Segments } from 'celebrate';
+import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow('').optional(),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+          return helpers.message('Invalid noteId');
+        }
+        return value;
+      }),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...TAGS).optional(),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+          return helpers.message('Invalid noteId');
+        }
+        return value;
+      }),
+  }),
+
+  [Segments.BODY]: Joi.object()
+    .keys({
+      title: Joi.string().min(1).optional(),
+      content: Joi.string().allow('').optional(),
+      tag: Joi.string().valid(...TAGS).optional(),
+    })
+    .min(1),
+};
